9dae7a7a063254ca8cf2d0de4b5e17079534a35c
pekriska <pekriska@gmail.com>
2023-12-06 19:39:40 +0100

    1.0.3
    home.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,7 +11,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header singel-page">
-		<p class="page-sub-title">— Aktuality —</p>
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title page-title">', '</h1>' );
